5f9d5295497ebe3e976437eb6dcdcae306c39549
iamjiery <54244163+iamjiery@users.noreply.github.com>
2019-10-30 22:32:40 +0800

    Update InputHandler.java

--- src/Assignment/InputHandler.java
+++ src/Assignment/InputHandler.java
@@ -28,4 +28,19 @@ public class InputHandler {
 		return input;
 	}
 
+	public static float priceInput(int lowRange, int highRange) {
+		Scanner scan = new Scanner(System.in);
+		while (!scan.hasNextFloat()){
+			System.err.println("Error... Please input a float:");
+			scan.next();
+		}
+		float choice = scan.nextFloat();
+		if (choice < (float)lowRange || choice > (float)highRange) {
+			System.err.printf("Error... Please input a float between %d-%d:\n",lowRange,highRange);
+			return priceInput(lowRange,highRange);
+		}
+		else{
+		return choice;
+		}
+	}
 }
